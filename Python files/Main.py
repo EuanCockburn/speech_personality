4cc8d1f984b01bbfa1bdab290d086d0ad4d35042
Loptr250 <cockburne@gmail.com>
2015-12-07 01:20:31 +0000

    Attempted to add Energy readings

--- Python files/Main.py
+++ Python files/Main.py
@@ -8,6 +8,9 @@ from ReadProsodyFiles import *
 from PlotHistogram import *
 from Extract_pitch import *
 from Extract_loudness import *
+from RunEnergy import *
+from ReadEnergy import *
+from Extract_energy import *
 
 # Retrieve the names of all the .wav files
 filenames = getfilenames()
@@ -20,6 +23,8 @@ RunEnergyConfig(filenames)
 
 # Read the data from Prosody Shs CSV files
 ProsShs_F0final, ProsShs_Voicingunclip, ProsShs_loudness = readProsodyShs()
+# Read the data from energy CSV files
+Energy = readEnergy()
 
 # Extract the averaged pitch from the Prosody data files
 ProsShs_F0final_noZero_Avg = RetrievePitch(ProsShs_F0final)
@@ -27,9 +32,13 @@ ProsShs_F0final_noZero_Avg = RetrievePitch(ProsShs_F0final)
 # Extract the averaged loudness from the Prosody data files
 ProsShs_loudness_avg = RetrieveLoudness(ProsShs_loudness)
 
+# Extract the averaged energy from the Energy data
+Energy_avg = RetrieveEnergy(Energy)
+
 # Plot the histogram of the pitch
 plotHistogram(ProsShs_F0final_noZero_Avg, "Natural Frequency", "Hz")
 plotHistogram(ProsShs_loudness_avg, "loudness", " ")
+plotHistogram(Energy_avg, "Energy", " ")
 
 
 
