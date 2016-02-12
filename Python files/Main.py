977a9dc4fd49970cc317921e0fdbe344b0deafc7
Loptr250 <cockburne@gmail.com>
2016-02-12 17:36:57 +0000

    Used a new configuration file, changed python code appropriately, currently attempting to read new csv files

--- Python files/Main.py
+++ Python files/Main.py
@@ -4,33 +4,14 @@ __author__ = 'Euan Cockburn'
 
 from RunExtraction import *
 from Extract_file_names import *
+from ReadCSV import *
 
 # Retrieve the names of all the .wav files
 filenames = getfilenames()
 
 # Run feature extraction configuration file on .wav files
-FeatureExtract(filenames)
+#FeatureExtract(filenames)
 
-# Read the data from the pitch files
-#Pitch, jitter, shimmer, Formant1, Formant2, Formant3 = readPitch()
-
-# Extract the averaged pitch and pitch jitter from the Pitch data files
-#Pitch_avg, jitter_avg, shimmer_avg, Formant1_avg, Formant2_avg, Formant3_avg = RetrievePitch(Pitch, jitter, shimmer, Formant1, Formant2, Formant3)
-
-# Extract the averaged loudness from the Prosody data files
-#Loudness_avg = RetrieveLoudness(Loudness)
-
-# Extract the averaged energy from the Energy data
-#Energy_avg = RetrieveEnergy(Energy)
-
-# Plot the histogram of the pitch
-#plotHistogram(Pitch_avg, "Natural Frequency", "Hz")
-#plotHistogram(Loudness_avg, "loudness", " ")
-#plotHistogram(Energy_avg, "Energy", " ")
-#plotHistogram(jitter_avg, "Jitter", " ")
-#plotHistogram(shimmer_avg, "Shimmer", " ")
-#plotHistogram(Formant1_avg, "Formant 1", " ")
-#plotHistogram(Formant2_avg, "Formant 2", " ")
-#plotHistogram(Formant3_avg, "Formant 3", " ")
-
+# Extract contents of CSV files
+ExtractCSV()
 
