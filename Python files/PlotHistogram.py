cfe920ba1c492a81df6cc7b5f35f0dae3755c3d2
Loptr250 <cockburne@gmail.com>
2016-02-25 10:31:37 +0000

    Plotted histograms

--- Python files/PlotHistogram.py
+++ Python files/PlotHistogram.py
@@ -10,4 +10,4 @@ def plotHistogram(Data, Title, Scale):
 	plt.title(Title)
 	plt.xlabel(Scale)
 	plt.ylabel("Frequency")
-	plt.savefig("/home/euan/Documents/university/year4/independant_project/speech_personality/Plots/" + Title +".png")
+	plt.savefig("/home/euan/Documents/university/year4/speech_personality/Plots/" + Title +".png")
