cfa79a1d3b51225fa16559d4dbafceb4ea969236
Loptr250 <cockburne@gmail.com>
2016-03-24 18:52:10 +0000

    Meaningful message

--- Python files/RunExtraction.py
+++ Python files/RunExtraction.py
@@ -8,7 +8,7 @@ import os
 def FeatureExtract(files):
     # For each file
     for f in range(len(files)):
-        # Create bash command
-	bashCommand = "sudo SMILExtract -C /home/euan/Documents/university/year4/speech_personality/MyConfig/gemaps/eGeMAPSv01a.conf -I " + files[f] + " -O /home/euan/Documents/university/year4/speech_personality/CSVs/eGeMAPSv01a/" + os.path.basename(files[f]) + ".csv"
-	# Run bash command
-	os.system(bashCommand)
+        # Create command
+	Command = "sudo SMILExtract -C /home/euan/Documents/university/year4/speech_personality/MyConfig/gemaps/eGeMAPSv01a.conf -I " + files[f] + " -O /home/euan/Documents/university/year4/speech_personality/CSVs/eGeMAPSv01a/" + os.path.basename(files[f]) + ".csv"
+	# Run command
+	os.system(Command)
